0e5212234f55723aa40e184417e29d9a4eecf8e9
cWeckner <skill3d.waffle@gmail.com>
2016-02-01 09:36:20 +0100

    Hehe, jadu

--- GithubTestGrejForProgrammeringsKurs2ForGymnasieeleverHiMomDeathToAmerica/src/PaketoNumeroUnoDeathToAmericaTwoElectricBuggaloo/IchHabeSaeurkrautInMeinemLederhausen.java
+++ GithubTestGrejForProgrammeringsKurs2ForGymnasieeleverHiMomDeathToAmerica/src/PaketoNumeroUnoDeathToAmericaTwoElectricBuggaloo/IchHabeSaeurkrautInMeinemLederhausen.java
@@ -3,6 +3,7 @@ package PaketoNumeroUnoDeathToAmericaTwoElectricBuggaloo;
 public class IchHabeSaeurkrautInMeinemLederhausen {
 
 	public static void main(String[] args) {
-		
+		//För länge länge sedan, I en galax lång långt borta
+		//Om du letar efter porren så skulle du svängt vänster vid Södertälje :P
 	}
 }
